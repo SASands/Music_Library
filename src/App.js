b9eb50b10cec6a46ac81407d38c1be96af215a49
SASands <snads@live.com>
2022-04-27 16:10:46 -0400

    Created component folders with .jsx and .css files + Created my initial table for music + installed axios + made my first hook for get all songs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+
+
 
 function App() {
+  const [songs, setSongs] = useState([])
+
+
+  // with the optional empty array in the last line of this useEffect it will only run the first time the page renders
+  // if we remove the optional array it will run once when the page renders and run every time it sees a variable change value on this component
+  // if we add a variable name in the optional array it will run when page renders or when that value changes 
+useEffect(() => {
+  getAllSongs();
+ 
+},[])
+  
+  async function getAllSongs(){  // grabbing all song data
+    // await is telling react that what is happening in this function might take longer than a normal function 
+    // response is catching the data being sent back from the Django API
+    // .get is our method type, we would need to say .put, .delete .post right after axios to change the type of request
+    let response = await axios.get('http://127.0.0.1:8000/api/music/');
+    console.log("ran axios request")
+    console.log(response.data);
+    setSongs(response.data);
+
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Album_Image</th>
+              <th>Title</th>
+              <th>Artist</th>
+              <th>Album</th>
+              <th>Release Date</th>
+              <th>Genre</th>
+              <th>Likes</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+  
+  
   );
 }
 
